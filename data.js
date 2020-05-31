1a98308c3a8425a31f434dda0ccbf97064bc3891
SATOSHI-HUANG <coming0302@gmail.com>
2020-05-31 17:54:47 +0800

    建立假資料

---
+++ data.js
@@ -0,0 +1,22 @@
+exports.users = [
+    { id: 1, name: 'Fong', age: 23, friendIds: [2, 3] },
+    { id: 2, name: 'Kevin', age: 40, friendIds: [1] },
+    { id: 3, name: 'Mary', age: 18, friendIds: [1] }
+];
+
+exports.books = [
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton',
+    },
+];
+
+exports.posts = [
+    { id: 1, authorId: 1, title: "Hello World!", content: "This is my first post.", likeGiverIds: [2] },
+    { id: 2, authorId: 2, title: "Good Night", content: "Have a Nice Dream =)", likeGiverIds: [2, 3] },
+    { id: 3, authorId: 1, title: "I Love U", content: "Here's my second post!", likeGiverIds: [] },
+];
